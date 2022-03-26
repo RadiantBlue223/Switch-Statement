8ad30b75d324e40ca76fd0ff31e4c17b64d80587
RadiantBlue223 <Landsberger_Mike@student.mahoningctc.com>
2022-03-26 15:16:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var yes = document.getElementsByClassName("correct");
+var no = document.getElementsByClassName("wrong");
+
+function go() {
+    document.getElementById('qwe').innerHTML = "Correct!"
+}
+function go1() {
+    document.getElementById('qwe1').innerHTML = "Correct!"
+}
+function go2() {
+    document.getElementById('qwe2').innerHTML = "Correct!"
+}
+function go3() {
+    document.getElementById('qwe3').innerHTML = "Correct!"
+}
+function shoot() {
+    document.getElementById('qwe').innerHTML = "Wrong!"
+}
+function shoot1() {
+    document.getElementById('qwe1').innerHTML = "Wrong!"
+}
+function shoot2() {
+    document.getElementById('qwe2').innerHTML = "Wrong!"
+}
+function shoot3() {
+    document.getElementById('qwe3').innerHTML = "Wrong!"
+}
